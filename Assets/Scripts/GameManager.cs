8d65b41923066b092235db11309a18529ea541cd
9003857 <9003857@alumnos.ufv.es>
2022-03-26 23:02:59 +0100

    TODO EL PUTO NIVEL 1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public GameObject[] go;
     public GameObject[] spawns;
-    private GameObject[] instance = new GameObject[1000];
+    private GameObject instanceobject;
     private List<Vector3> spawnscheck;
     private bool encontrarposicion = false;
     private bool spawnbool = true;
@@ -16,11 +16,31 @@ public class GameManager : MonoBehaviour
     public float t_respawn = 1;
     //Intervalo de repeticion del spawner
     public float tRespawn = 1;
-    // Start is called before the first frame update
-    void Start()
+    public bool[] posicionesocupadas = new bool[4] { false, false, false, false };
+    public static float dropTime = 1f;
+    public GameObject[] blocks;
+    bool caida = true;
+    float aparacionBloque;
+
+    public int countinstance=0;
+    private static GameManager instance;
+    public static GameManager Instance
     {
+        get
+        {
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
         InvokeRepeating("spawnObject", 3f, 3f);
-        nextPoint = 0;
+        Debug.Log(countinstance);
 
         
     }
@@ -28,37 +48,60 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (caida)
+        {
+            spawn();
+            StartCoroutine(Block());
+        }
 
+    }
+    public void spawn()
+    {
+        float escogerBloque = Random.Range(0, 1f);
+        escogerBloque *= blocks.Length;
+        var position = new Vector3(Random.Range(-2.0f, 2.0f), 5f, 0);
+        Instantiate(blocks[Mathf.FloorToInt(escogerBloque)], position, Quaternion.identity);
+    }
+    IEnumerator Block()
+    {
+        caida = false;
+        yield return new WaitForSeconds(1);
+        caida = true;
     }
     private void spawnObject()
     {
-        //una posicion aleatoria de los waypoints
-        Vector3 posicion = spawns[Random.Range(0, spawns.Length)].transform.position;
-        spawnbool = true;//esto para entrar en el bucle por si acaso
-        while (spawnbool == true)
+        if (countinstance > 3)
         {
 
-            spawnbool = false;//esto para slair del bucle en caso de no encontrar un objeto en la misma posicion
-            for (int x = 0; x < instance.Length; x++)//bucle recorriendo todos los objetos instanciados
-            {
-                if (instance[x]!=null &&instance[x].transform.position == posicion)//si ese objeto no es null y su posicion es igual a la del principio dle metodo
-                {
-                    spawnbool = true;//ponemos esto a true para seguir el bucle
-                    break;//salimos del for
-                }
-            }
-            if (spawnbool == true)//si es true
-            {
-                posicion = spawns[Random.Range(0, spawns.Length)].transform.position;//ponemos otra posicion aleatoria de los waypoints
-            }
-
         }
-        Instantiate(go[Random.Range(0, go.Length)], posicion, Quaternion.identity);//una vez salimos dle bucle, instanciamos el objeto en esa posicion
+        else
+        {
+            //una posicion aleatoria de los waypoints
+            int posicionint = Random.Range(0, spawns.Length);
+            
+            //esto para entrar en el bucle por si acaso
+            while (posicionesocupadas[posicionint]==true)
+            {
+
+                posicionint = Random.Range(0, spawns.Length);
+
+            }
+            Vector3 posicion = spawns[posicionint].transform.position;
+            Debug.Log(posicionint);
+            posicionesocupadas[posicionint] = true;
+            instanceobject=Instantiate(go[Random.Range(0, go.Length)], posicion, Quaternion.identity);//una vez salimos dle bucle, instanciamos el objeto en esa posicion
+            instanceobject.GetComponent<Objeto>().posicion = posicionint;
+            countinstance++;
+        }
+        
 
 
 
 
-
+    }
+    public void eliminarObjeto(int num)
+    {
+        countinstance--;
+        posicionesocupadas[num] = false;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public bool dch = false;
     public bool izq = false;
     public bool jump = false;
-
+    public int salto;
     public Rigidbody2D rb;
     public float speedForce;
-    public float salto;
-
+    public Text textovida;
     public int vida;
-
-
+    public GameObject pelo;
+    public GameObject cara;
+    public GameObject brazo1;
+    public GameObject brazo2;
+    public Image t1, t2, t3;
+    private Color[] color= {new Color(1, 1, 1, 1),new Color(1, 1, 0, 1), new Color(0, 0, 0, 1), new Color(0.5f, 0.5f, 0.5f, 1), new Color(118, 57, 31, 255)};
+    private Color[] colorpiel = { new Color(255, 255, 255, 255),  new Color(118, 57, 31, 255), new Color(67,30,16, 255), new Color(203, 134, 108, 255) };
+    //-1.69, -4.637184
+    public bool tool1, tool2, tool3;
     public void Saveplayervida()
     {
         //vida = 100;
@@ -30,9 +37,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
+   
     public void Start()
     {
+       
         Loadplayervida();
 
         if(vida <= 0)
@@ -85,17 +93,65 @@ public class PlayerController : MonoBehaviour
             //rb.AddForce(new Vector2(0, salto));
             transform.position += new Vector3(0f, salto, 0f) * Time.deltaTime;
         }
-
+        
     }
 
-
+    private void ganar()
+    {
+        if(tool1==true && tool2==true && tool3 == true)
+        {
+            PlayerPrefs.GetInt("Qatar", 1);
+        }
+    }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Object")
+        if (collision.gameObject.tag == "Tool1" || collision.gameObject.tag == "Tool2" || collision.gameObject.tag == "Tool3" || collision.gameObject.tag == "agua")
         {
             Destroy(collision.gameObject);
+            GameManager.Instance.eliminarObjeto(collision.gameObject.GetComponent<Objeto>().posicion);
+            if (collision.gameObject.tag == "Tool1")
+            {
+                tool1 = true;
+                t1.enabled = true;
+            }
+            else if (collision.gameObject.tag == "Tool2")
+            {
+                tool2 = true;
+                t2.enabled = true;
+            }
+            else if (collision.gameObject.tag == "Tool3")
+            {
+                tool3 = true;
+                t3.enabled = true;
+            }
+            else if (collision.gameObject.tag == "agua")
+            {
+                vida += 10;
+            }
+        }
+        else if (collision.gameObject.tag == "ladrillo")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("UWU");
+            vida = vida - 20;
+            if (vida <= 0)
+            {
+                Color piel= color[Random.Range(0, color.Length)];
+                transform.position = new Vector3(-1.69f, -4.637184f, 0);
+                vida = 100;
+                PlayerPrefs.SetInt("vidaPlayer", vida);
+                pelo.GetComponent<SpriteRenderer>().color = color[Random.Range(0, color.Length)];
+                cara.GetComponent<SpriteRenderer>().color = piel;
+                brazo1.GetComponent<SpriteRenderer>().color = piel;
+                brazo2.GetComponent<SpriteRenderer>().color = piel;
+                t1.enabled = false;
+                t2.enabled =  false;
+                t3.enabled = false;
+
+            }
+            
         }
     }
 }
