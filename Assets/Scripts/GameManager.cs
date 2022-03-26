a0753574efe5640f60bba98842add82cef9443a3
9003857 <9003857@alumnos.ufv.es>
2022-03-26 23:20:41 +0100

    NIVEL1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,6 +38,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+
         instance = this;
         InvokeRepeating("spawnObject", 3f, 3f);
         Debug.Log(countinstance);
