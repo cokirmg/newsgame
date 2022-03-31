4b76954aa0a2d2988c8b82d61409c8235601ee30
cokirmg <rodrigomg666@gmail.com>
2022-03-31 20:40:04 +0200

    qatar terminado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -35,6 +36,119 @@ public class PlayerController : MonoBehaviour
     public int numestadio=0;
     //-1.69, -4.637184
     public bool tool1, tool2, tool3;
+
+    public string[] nombres =
+    {
+        "Raúl Vargas",
+        "Benito Antonio Martínez Ocasio",
+        "Joaquín Gúzman",
+        "Enmanuel Gazmey Santiago",
+        "Ramón Ayala Rodríguez",
+        "Kamal Laskar",
+        "William Omar Landrón Rivera",
+        "Khalid Malik Ahmad",
+        "Syed Shah",
+
+
+
+
+    };
+    public string returnStringNombre;
+
+    public string[] fallecimientoaeruopuerto =
+    {
+        "2020",
+        "2021",
+    };
+    public string[] fallecimientoqatar =
+    {
+        "2020",
+        "2021",
+        "2019",
+        "2018",
+        "2017",
+
+    };
+    public string returnStringfallq;
+
+    public string[] nacimiento =
+    {
+        "2000",
+        "1999",
+        "1998",
+        "1997",
+        "1996",
+        "1995",
+        "1994",
+        "1993",
+        "1992",
+        "1991",
+        "1990",
+        "1989",
+        "1988",
+        "1987",
+        "1986",
+        "1985",
+        "1984",
+        "1983",
+        "1982",
+        "1981",
+        "1980",
+        "1979",
+        "1978",
+        "1977",
+        "1976",
+        "1975",
+    };
+    public string returnStringnac;
+
+    public string[] causas =
+    {
+        "Causa de muerte: Debido a problemas de corazón por causas naturales \n " +
+        "Datos de empleado: Su familia asegura que siempre tuvo buena salud, no le hicieron autopsia y más tarde les indemnizaron. ",
+        "Causa de muerte: Problemas de corazón por causas naturales \n" +
+        "Datos de empleado: Su familia asegura que tuvo buena salud antes de su muerte, no le hicieron autopsia y más tarde les indemnizaron.",
+        "Causa de muerte: Problemas de corazón por causas naturales \n" +
+        "Datos de empleado: Su familia asegura que tuvo buena salud antes de su muerte, no le hicieron autopsia y más tarde les indemnizaron.",
+        "Causa de muerte: Dificultades respiratorias por causas naturales \n" +
+        "Datos de empleado: Se trasladado al hospital y fue declarado muerto. Más tarde indemnizaron a su familia. Aseguran que murió mientras trabajaba",
+        "Causa de muerte: Insuficiencia cardiorrespiratoria por causas naturales \n" +
+        "Datos de empleado: Estuvo trabajando a 39 grados durante más de 8 horas. Nadie les contactó tras su fallecimiento, no le hicieron autopsia y más tarde les indemnizaron.",
+        "Causa de muerte: Problemas de corazón no específicos \n" +
+        "Datos de empleado: Estuvo trabajando durante 13 horas, nadie les contactó tras su fallecimiento ni le hicieron autopsia y más tarde les indemnizaron.",
+        "Causa de muerte: Insuficiencia cardiorrespiratoria por causas naturales \n" +
+        "Datos de empleado: Estuvo trabajando durante 13 horas, su familia asegura que siempre tuvo buena salud. No llegó a tiempo al hospital.",
+        "Causa de muerte: Contusión craneoencefálica debido al aplastamiento de la caída de una grúa mal asegurada. \n" +
+        "Datos del empleado: Joven sin estudios con siete hermanos pequeños. Le prometieron un mayor salario, sin embargo no tenían la seguridad adecuada.",
+        "Causa de muerte: Caída a diferente nivel por falta de aseguramiento de los arneses. \n"+
+        "Datos del empleado: Trabajaba en un turno de noche, lo que provocó que falleciera por la falta de atención médica durante esas horas",
+        "Causa de muerte: Multitud de traumatismos por atropellamiento con equipo móvil, motoniveladora.\n" +
+        "Datos del empleado: Padre de una niña. La empresa se negaba a reconocer su muerte.",
+        "Causa de muerte: Fractura craneoencefálica por aplastamiento al caerle unas vigas encima \n" +
+        "Datos del empleado: La empresa dijo que fue un error en el uso de las máquinas como justificación para no pagar la indemnización. ",
+        "Causa de muerte: Traumatismo craneoencefálico, policontusión torácica y fractura en parrilla costal.\n" +
+        "Datos del empleado: Le pusieron horas extras encargándose de maquinaria fuera de su profesión. ",
+        "Causa de muerte: Multitud de contusiones por volcadura de vehículo en excavación. \n" +
+        "Datos del empleado: La construcción no mantenía las condiciones de trabajo y era forzado a trabajar más de las horas establecidas.",
+        "Causa de muerte: múltiples quemaduras en el cuerpo.\n" +
+        "Datos del empleado: Le obligaron a trabajar más horas de las establecidas, tras morir su hija acabó en un orfanato. ",
+        "Causa de muerte: Paro cardíaco por una descarga eléctrica.\n" +
+        "Datos del empleado: Empezó a trabajar en la obra para conseguir formar una familia. A su mujer le llegó una llamada informando del fallo eléctrico y el fallecimiento de su esposo.",
+
+
+
+
+
+
+    };
+    public string returnStringCaus;
+    public TextMeshProUGUI textNacimiento;
+    public TextMeshProUGUI textFallecimientoqatar;
+    public TextMeshProUGUI textFallecimientoaeropuerto;
+    public TextMeshProUGUI textNombres;
+    public TextMeshProUGUI textCase;
+    public TextMeshProUGUI textCausas;
+
     public void Saveplayervida()
     {
         //vida = 100;
@@ -222,6 +336,19 @@ public class PlayerController : MonoBehaviour
     {
         fondoMuerte.SetActive(true);
         fichaCasos.SetActive(true);
+        returnStringNombre = nombres[Random.Range(0, nombres.Length)];
+        returnStringnac = nacimiento[Random.Range(0, nacimiento.Length)];
+        returnStringfallq = fallecimientoqatar[Random.Range(0, fallecimientoqatar.Length)];
+        returnStringCaus = causas[Random.Range(0, causas.Length)];
+
+
+        textCausas.text = returnStringCaus;
+        textCase.text = Random.Range(0, 6500) + " ";
+        textNombres.text = returnStringNombre;
+        textNacimiento.text = returnStringnac;
+        textFallecimientoqatar.text = returnStringfallq;
+
+
         Time.timeScale = 0;
         
     }
